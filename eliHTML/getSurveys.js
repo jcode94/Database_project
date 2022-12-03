d7e5c36dfcb906f85618114881cfdf174ca70420
fergy-custom <fergyfive55@gmail.com>
2022-12-02 22:44:08 -0500

    Update

--- eliHTML/getSurveys.js
+++ eliHTML/getSurveys.js
@@ -4,21 +4,61 @@ function getSurveys()
 {
     console.log("Here")
 
-    let surveyArea = document.getElementById('surveyArea')
+    let surveyList = document.getElementById('surveyList')
 
-    for (let index = 0; index < 10; index++) {
-        surveyArea.innerHTML = surveyArea.innerHTML + 
-        '<div id="" class="col-auto color">'+
-            '<div class="col">'+
-                '<div>'+
-                    '<p>Survey Name</p>'+
-                '</div>'+
-                
-            '</div>'+
-            '<div class="col">'+
-                '<p>Questions: ###</p>'+
-                '<p>New</p>'+
-            '</div>'+
-       ' </div>'       
+    let jsonSurveys = {
+        0:{
+            surveyName:"Zero",
+            numberOfQuestions:10,
+            completionStatus:"Pending"
+        },
+        1:{
+            surveyName:"One",
+            numberOfQuestions:25,
+            completionStatus:"Completed"
+        },
+        2:{
+            surveyName:"Two",
+            numberOfQuestions:16,
+            completionStatus:"In-Progress"
+        },
+        3:{
+            surveyName:"Three",
+            numberOfQuestions:30,
+            completionStatus:"Pending"
+        }
     }
+
+    let surveyIds=[]
+    let surveyNames=[]
+    let surveyQuestionNumbers=[]
+    let surveyStatuses=[]
+
+    Object.keys(jsonSurveys).forEach(surveyId=>(
+            surveyIds.push(surveyId),
+
+            surveyNames.push(jsonSurveys[surveyId].surveyName),
+            surveyQuestionNumbers.push(jsonSurveys[surveyId].numberOfQuestions),
+            surveyStatuses.push(jsonSurveys[surveyId].completionStatus)
+
+        ))
+
+    console.log(
+        surveyIds,
+        surveyNames,
+        surveyQuestionNumbers,
+        surveyStatuses
+    )
+
+    for( let idx = 0; idx < surveyIds.length; idx++ )
+    {
+        surveyList.innerHTML = surveyList.innerHTML + 
+        '<div class="col-auto box clickable bd">\n' +
+            '\t<div class="row"><p>' + surveyNames[idx] + '</p></div>\n' +
+            '\t<div class="row"><p>Questions: ' + surveyQuestionNumbers[idx] + '</p></div>\n'+
+            '\t<div class="row"><p>' + surveyStatuses[idx] + '</p></div>\n'+
+        '</div>\n'
+    }
+    console.log(surveyList)
+    
 }
