6f72e88a2acce0a62da0a2ab8eed00c84f75c9cd
Justice Smith <jcode94ttv@gmail.com>
2022-12-03 20:09:41 -0500

    Added CRUD queries

---
+++ backend/SQL/CREATE_TABLE_USERS.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    email varchar(30) NOT NULL,
+    password varchar(30) NOT NULL,
+PRIMARY KEY(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
