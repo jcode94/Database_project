bf2a998561c8c96d987c7c408a198d4d255c0ae3
fergy-custom <fergyfive55@gmail.com>
2022-12-04 04:20:15 -0500

    easy form val for login

--- JS/login.js
+++ JS/login.js
@@ -1,16 +1,43 @@
 function doLogin() {
-    // console.log("Login Button Clicked")
 
-    // window.location.href = "./HTML/homePage.html";
-    // return
+    let flag = false
 
     let urlBase = "http://157.245.93.19/backend/api";
     let extension = ".php";
 
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
 
-    console.log(email, password)
+    if( email.value.trim() == '' )
+    {
+        flag=true
+        email.style.borderColor = 'red'
+        
+        email.addEventListener("focus", ()=>{
+            email.style.borderColor = 'black'
+        })
+    }
+    else
+    {
+        email = email.value
+    }
+
+    if( password.value.trim() == '' )
+    {
+        flag=true
+        password.style.borderColor = 'red'
+        
+        password.addEventListener("focus", ()=>{
+            password.style.borderColor = 'black'
+        })
+    }
+    else
+    {
+        password = password.value
+    }
+
+    // Missing either email or password -> return
+    if(flag) {return}
 
     //* Variables for the http request to login with the login api
     let jsonPayLoad = JSON.stringify({
