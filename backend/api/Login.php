9d0016c7b7b965575a5244fc9c191c7ff8202771
Justice Smith <jcode94ttv@gmail.com>
2022-12-04 03:15:14 -0500

    elim post, json read only

--- backend/api/Login.php
+++ backend/api/Login.php
@@ -1,10 +1,15 @@
 <?php
-$email = $_POST['email'];
-$password = $_POST['password'];
+require_once(__DIR__ . '/../../../config/Config.class.php');
+define('__BACKEND_ROOT__', $_SERVER['DOCUMENT_ROOT'].'/backend');
+require_once(__BACKEND_ROOT__.'/dao/DBConnection.php');
+$data = json_decode(file_get_contents("php://input"), true);
+$email = $data["email"];
+$password = $data["password"];
+$db_config = new Config();
 $conn = new DBConnection();
 
 if (empty($email) || empty($password)) {
-    echo 'empty field';
+    echo 0;
     exit;
 } else {
     if ($stmt = $conn->prepare("SELECT `uid`, `password` FROM `users` WHERE `email` = ?")) {
@@ -13,16 +18,16 @@ if (empty($email) || empty($password)) {
         $stmt->store_result();
         
         if ($stmt->num_rows > 0) {
-            $stmt->bind_result($uid, $password);
+            $stmt->bind_result($uid, $pw);
             $stmt->fetch();
-            if ($_POST['password'] === $password)) {
+            if ($password === $pw)) {
                 // Verification success! User has logged-in!
                 // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
                 session_regenerate_id();
                 $_SESSION['loggedin'] = TRUE;
                 $_SESSION['email'] = $_POST['email'];
                 $_SESSION['uid'] = $uid;
-                echo 'success';
+                echo 1;
                 exit;
             }
         }
