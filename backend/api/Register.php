f69f000f7dd4168d51fb42f2cb690a48d0a46143
Justice Smith <jcode94ttv@gmail.com>
2022-12-03 23:43:17 -0500

    verbose errors

--- backend/api/Register.php
+++ backend/api/Register.php
@@ -8,23 +8,23 @@ $password = $_POST['password'];
 $conn = new DBConnection();
 
 if (empty($email) || empty($password)) {
-    echo 0;
+    echo 'empty field';
 } else {
     $query = "SELECT `email` FROM `users` where `email` = $email";
     $rs = $conn->query($query);
     if (mysqli_num_rows($rs) > 0) {
-        echo(0);
+        echo 'already exists';
     } else {
         if ($_server["REQUEST_METHOD"] == "POST") {
             $stmt = $conn->prepare(file_get_contents(__BACKEND_ROOT__.'/SQL/INSERT_NEW_USER.sql'));
             $stmt->bind_param("is", $email, $password);
             if($stmt->execute()) {
-                echo 1;
+                echo 'success';
                 exit;
             } 
         }
     }
 }
 
-echo 0;
+echo 'fail';
 ?>
