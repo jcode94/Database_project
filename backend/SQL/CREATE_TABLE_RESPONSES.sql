5219e0f286278f07047befeb6a404b11ca5e4945
Justice Smith <jcode94ttv@gmail.com>
2022-12-03 22:45:47 -0500

    table responses, field value nullable

--- backend/SQL/CREATE_TABLE_RESPONSES.sql
+++ backend/SQL/CREATE_TABLE_RESPONSES.sql
@@ -2,6 +2,6 @@ CREATE TABLE `responses` (
 	`survey_id` INT NOT NULL,
 	`email` VARCHAR(30) NOT NULL,
 	`order` INT NOT NULL,
-	`value` VARCHAR,
+	`value` VARCHAR DEFAULT NULL,
 	PRIMARY KEY (`survey_id`,`email`,`order`)
 ) ENGINE=InnoDB;
