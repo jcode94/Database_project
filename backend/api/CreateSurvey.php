d7d8198d256fd8a138237a07abf9e32a4ee23649
Justice Smith <jcode94ttv@gmail.com>
2022-12-05 16:57:20 -0500

    print

--- backend/api/CreateSurvey.php
+++ backend/api/CreateSurvey.php
@@ -35,8 +35,6 @@ $continue = $stmt->execute();
 
 $survey_id = $conn->query("SELECT `survey_id` FROM `surveys_metadata` ORDER BY `survey_id` DESC LIMIT 1");
 
-echo $survey_id;
-
 if (isset($continue)) {
     // INSERT INTO QUESTIONS (survey_id, number, type, statement)
     foreach ($data['questions'] as $index => $question) {
