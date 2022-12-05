0011eea942f10a25376ed6134ca0ba12157ff887
Justice Smith <jcode94ttv@gmail.com>
2022-12-05 13:23:58 -0500

    add to error log

--- backend/api/CreateSurvey.php
+++ backend/api/CreateSurvey.php
@@ -4,8 +4,8 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/backend/models/Constants.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/../config/Config.class.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/backend/dao/DBConnection.php');
 $data = json_decode(file_get_contents("php://input"), true);
-print_r($data);
-echo $data["author"];
+error_log(print_r($data));
+error_log($data["author"]);
 // insert survey into table surveys_metadata, get back its ID
 $conn = new DBConnection(new Config());
 $stmt = $conn->prepare(
