9b7761c4ed9feefa54817a31fbd468e1dfccc853
fergy-custom <fergyfive55@gmail.com>
2022-12-05 14:13:34 -0500

    Update register.js file

--- JS/register.js
+++ JS/register.js
@@ -2,28 +2,26 @@
 
 function doRegister()
     {        
-        console.log("Register Button Clicked")
+        console.log("Register Button Clicked")        
 
-        window.location.href = "./HTML/homePage.html";
-        
+        let urlBase = "http://157.245.93.19/backend/api";
+        let extension = '.php';
 
-        urlBase = 'http://157.245.93.191';
-        extension = '.php';
-
-        this.email = document.getElementById('email').value;
-        this.password = document.getElementById('password').value;
+        let email = document.getElementById('email').value;
+        let password = document.getElementById('password').value;
         
         
         //* Variables for the http request to login with the login api
         let jsonPayLoad = JSON.stringify(
             {
-                email:this.email,
-                password:this.password
+                email:email,
+                password:password
             });
-            // logging the payload being sent 
-            console.log("Stringify sent: " + jsonPayLoad);
 
-        let url = this.urlBase + "/Register" + this.extension;
+        // logging the payload being sent 
+        console.log("Stringify sent: " + jsonPayLoad);
+
+        let url = urlBase + "/Register" + extension;
         let method = "POST";
 
         //* Opening the connection to the login api file with the login & password typed in
@@ -31,33 +29,37 @@ function doRegister()
         xhr.open(method, url, true);
         xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 
-        try
-        {
-            let that = this;
-            xhr.onreadystatechange = function()
-            {
-                if(this.readyState == 4 && this.status == 200)
-                {
+        try {
+            xhr.onreadystatechange = function () {
+    
+                // If server pinged and a response is sent back
+                if (this.readyState == 4 && this.status == 200) {
+    
+                    console.log('Received', xhr.responseText)
+    
                     let jsonObject = JSON.parse(xhr.responseText);
-
-                    if(that.userId < 1)
+    
+                    console.log('JSON Received', jsonObject)
+    
+                    if ( jsonObject.valid == "valid")
                     {
-                        document.getElementById("loginResult").innerHTML = "Username or Password are incorrect";
-                        return;
+                        sessionStorage['userEmail'] = email
+                        console.log( "Session Storage", sessionStorage )
+    
+                        document.getElementById('loginResult').innerText = "Successful Login"
+    
+                        window.location.href = "./HTML/homePage.html";
                     }
-
-                    let Cookie = new cookie(that.email);
-                    Cookie.saveCookie();
-
-                    window.location.href = "homePage.html";
+                    else
+                    {
+                        document.getElementById('loginResult').innerText = jsonObject.valid
+                    }
                 }
             };
-
-            xhr.send(jsonPayLoad);
-        }
-        catch(err)
-        {
+    
+            xhr.send(jsonPayLoad)
+    
+        } catch (err) {
             document.getElementById("loginResult").innerHTML = err.message;
         }
-        return
     }
