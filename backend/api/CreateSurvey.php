9be6c2ee04dd64197ba706a0e7583d9a01bbb9bf
Justice Smith <jcode94ttv@gmail.com>
2022-12-05 14:32:07 -0500

    emit

--- backend/api/CreateSurvey.php
+++ backend/api/CreateSurvey.php
@@ -1,13 +1,13 @@
 <?php
-echo json_encode(print_r($_REQUEST));
+echo json_encode(print_r($_GET));
+echo json_encode(print_r($_POST));
+echo json_encode(print_r($_COOKIE));
 define('__BACKEND_ROOT__', $_SERVER['DOCUMENT_ROOT'] . '/backend');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/backend/models/Constants.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/../config/Config.class.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/backend/dao/DBConnection.php');
 
 $data = json_decode(file_get_contents("php://input"), true);
-echo json_encode($data['test']);
-echo json_encode($data["test"]);
 // insert survey into table surveys_metadata, get back its ID
 $conn = new DBConnection(new Config());
 $stmt = $conn->prepare(
