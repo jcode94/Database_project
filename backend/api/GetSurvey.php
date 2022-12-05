9e995a02c135fea41055ce51c992e645c5d26405
Justice Smith <jcode94ttv@gmail.com>
2022-12-05 18:07:16 -0500

    add

--- backend/api/GetSurvey.php
+++ backend/api/GetSurvey.php
@@ -3,6 +3,7 @@ define('__BACKEND_ROOT__', $_SERVER['DOCUMENT_ROOT'] . '/backend');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/backend/models/Constants.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/../config/Config.class.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/backend/dao/DBConnection.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/backend/models/Survey.php');
 $conn = new DBConnection(new Config());
 
 $data = json_decode(file_get_contents("php://input"), true);
