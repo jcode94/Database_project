a75576be1f3b0436957954051b0422af07f0c3af
Justice Smith <jcode94ttv@gmail.com>
2022-12-06 14:52:24 -0500

    print

--- backend/api/SubmitSurvey.php
+++ backend/api/SubmitSurvey.php
@@ -6,6 +6,8 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/../config/Config.class.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/backend/dao/DBConnection.php');
 
 $data = json_decode(file_get_contents("php://input"), true);
+echo json_encode($data);
+exit;
 $conn = new DBConnection(new Config());
 $survey_id = $data['survey_id'] ?? 0;
 $email = $data['email'] ?? "";
