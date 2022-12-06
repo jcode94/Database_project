699fa7097e7e7e6ce727fdb498a9b0d621b356dd
Justice Smith <jcode94ttv@gmail.com>
2022-12-05 20:11:54 -0500

    Questions

--- backend/api/GetSurvey.php
+++ backend/api/GetSurvey.php
@@ -1,6 +1,7 @@
 <?php
 define('__BACKEND_ROOT__', $_SERVER['DOCUMENT_ROOT'] . '/backend');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/backend/models/Constants.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/backend/models/Question.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/../config/Config.class.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/backend/dao/DBConnection.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/backend/models/Survey.php');
