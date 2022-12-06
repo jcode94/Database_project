3baad695790af4d8407ee27f78d70e3ccc70c04d
Justice Smith <jcode94ttv@gmail.com>
2022-12-05 23:40:25 -0500

    add

--- backend/dao/DBConnection.php
+++ backend/dao/DBConnection.php
@@ -1,49 +1,57 @@
 <?php
-    
-    
-    class DBConnection {
-        private $host;
-        private $username;
-        private $password;
-        private $database;
-    
-        protected $connection;
-    
-        public function __construct(Config $config){
-            
-            if (!isset($this->connection)) {
-                $this->host = $config->host;
-                $this->username = $config->username;
-                $this->password = $config->password;
-                $this->database = $config->database;
-                
-                $this->connection = new mysqli(
-                    $this->host, $this->username, $this->password, $this->database
-                );
-    
-                if (!$this->connection) {
-                    echo 'Cannot connect to database server';
-                    exit;
-                }            
-            }    
-    
-            return $this->connection;
+
+
+class DBConnection
+{
+    private $host;
+    private $username;
+    private $password;
+    private $database;
+
+    protected $connection;
+
+    public function __construct(Config $config)
+    {
+
+        if (!isset($this->connection)) {
+            $this->host = $config->host;
+            $this->username = $config->username;
+            $this->password = $config->password;
+            $this->database = $config->database;
+
+            $this->connection = new mysqli(
+                $this->host,
+                $this->username,
+                $this->password,
+                $this->database
+            );
+
+            if (!$this->connection) {
+                echo 'Cannot connect to database server';
+                exit;
+            }
         }
-        
-        public function query($sql) {
-            return $this->connection->query($sql);
-        }
-        
-        public function prepare($sql) {
-            return $this->connection->prepare($sql);
-        }
-        
-        public function __destruct()
-        {
-            mysqli_close($this->connection);
-        }
-        
-        public function next_result($sql) {
-            return $this->connection->next_result();
-        }
+
+        return $this->connection;
     }
+
+    public function query($sql)
+    {
+        return $this->connection->query($sql);
+    }
+
+    public function prepare($sql)
+    {
+        return $this->connection->prepare($sql);
+    }
+
+    public function __destruct()
+    {
+        mysqli_close($this->connection);
+    }
+
+    public function next_result()
+    {
+        return $this->connection->next_result();
+    }
+}
