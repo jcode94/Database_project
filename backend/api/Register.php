75d4a0dfeb2f4f2a57f52078221875cf04454d6f
Justice Smith <jcode94ttv@gmail.com>
2022-12-05 21:46:23 -0500

    jsonencode

--- backend/api/Register.php
+++ backend/api/Register.php
@@ -1,8 +1,8 @@
 <?php
 define('__BACKEND_ROOT__', $_SERVER['DOCUMENT_ROOT'] . '/backend');
-require_once($_SERVER['DOCUMENT_ROOT'].'/backend/models/Constants.php');
-require_once($_SERVER['DOCUMENT_ROOT'].'/../config/Config.class.php');
-require_once($_SERVER['DOCUMENT_ROOT'].'/backend/dao/DBConnection.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/backend/models/Constants.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/../config/Config.class.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/backend/dao/DBConnection.php');
 
 $data = json_decode(file_get_contents("php://input"), true);
 $email = $data["email"];
@@ -10,19 +10,19 @@ $password = $data["password"];
 $conn = new DBConnection(new Config());
 
 if (empty($email) || empty($password)) {
-    echo json_encode(["valid"=>"invalid"]);
+    echo json_encode(["valid" => "invalid"]);
     exit;
 } else {
     $query = "SELECT `email` FROM `users` where `email` = '$email'";
     $rs = $conn->query($query);
     if (mysqli_num_rows($rs) > 0) {
-        vsprintf()
+        echo json_encode(["valid" => "invalid"]);
         exit;
     } else {
-        $stmt = $conn->prepare(file_get_contents(__BACKEND_ROOT__.'/SQL/INSERT_INTO_USERS.sql'));
+        $stmt = $conn->prepare(file_get_contents(__BACKEND_ROOT__ . '/SQL/INSERT_INTO_USERS.sql'));
         $stmt->bind_param("ss", $email, $password);
-        if($stmt->execute()) {
-            echo json_encode(["valid"=>"valid"]);
+        if ($stmt->execute()) {
+            echo json_encode(["valid" => "valid"]);
             exit;
         }
     }
