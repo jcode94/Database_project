440e12d09cf91e562f8d3b6f8d8d30b5650e7e5d
Justice Smith <59818423+jcode94@users.noreply.github.com>
2022-12-09 21:20:55 -0500

    empty check alr handled on FE

--- backend/api/Login.php
+++ backend/api/Login.php
@@ -8,32 +8,25 @@ $data = json_decode(file_get_contents("php://input"), true);
 $email = $data["email"] ?? "";
 $password = $data["password"] ?? "";
 $conn = new DBConnection(new Config());
+$query = "SELECT `uid`, `password` FROM `users` WHERE `email` = ?";
+if ($stmt = $conn->prepare($query)) {
+    $stmt->bind_param('s', $email);
+    $stmt->execute();
+    $stmt->store_result();
 
-if (empty($email) || empty($password)) {
-    echo json_encode(["valid" => "Missing Password Or Email"]);
-    exit;
-} else {
-    if ($stmt = $conn->prepare("SELECT `uid`, `password` FROM `users` WHERE `email` = ?")) {
-        $stmt->bind_param('s', $email);
-        $stmt->execute();
-        $stmt->store_result();
-
-        if ($stmt->num_rows > 0) {
-            $stmt->bind_result($uid, $pw);
-            $stmt->fetch();
-            if ($password === $pw) {
-                // Verification success! User has logged-in!
-                // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
-                session_start();
-                $_SESSION['loggedin'] = TRUE;
-                $_SESSION['email'] = $email;
-                $_SESSION['uid'] = $uid;
-                echo json_encode(["valid" => "valid"]);
-                exit;
-            }
-        } else {
-            echo json_encode(["valid" => "Email Not Found"]);
+    if ($stmt->num_rows > 0) {
+        $stmt->bind_result($uid, $pw);
+        $stmt->fetch();
+        if ($password === $pw) {
+            session_start();
+            $_SESSION['loggedin'] = TRUE;
+            $_SESSION['email'] = $email;
+            $_SESSION['uid'] = $uid;
+            echo json_encode(["valid" => "valid"]);
             exit;
         }
+    } else {
+        echo json_encode(["valid" => "Email Not Found"]);
+        exit;
     }
 }
